42146c6f48d1b95d9999e17a4309195e3f790218
Kate Klavina <kate.klavina97@gmail.com>
2019-11-22 10:51:07 +0200

    homework

--- Persist Data Example/ViewController.swift
+++ Persist Data Example/ViewController.swift
@@ -9,12 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var counterLabel: UILabel!
+    
+    //Create a variable to store the initial integer value
+    var counter = 0
+    let defaults = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        counter = defaults.integer(forKey: "counterKey")
+        counterLabel.text = "\(counter)"
     }
+    override func didReceiveMemoryWarning() {
+    
+           super.didReceiveMemoryWarning()
+    
+           // Dispose of any resources that can be recreated.
+    
+       }
 
-
+    @IBAction func adOneButtonPressed(_ sender: Any) {
+        counter += 1
+        counterLabel.text = "\(counter)"
+        defaults.setValue(counter, forKey: "counterKey")
+        
+        
+    }
 }
 
